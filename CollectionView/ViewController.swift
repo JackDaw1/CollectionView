6cabf0e65629a82c3a302af5c92016ce5f3b48e0
JackDaw1 <iaroshenko.ga@gmail.com>
2022-12-07 20:41:11 +0700

    add files

---
+++ CollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by Galina Iaroshenko on 07.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
