0eab9ab4b8d915219470557c15c1a473ccd5ec81
JackDaw1 <iaroshenko.ga@gmail.com>
2022-12-09 12:07:46 +0700

    number cell collection view

---
+++ CollectionView/NumberCell.swift
@@ -0,0 +1,17 @@
+//
+//  NumberCell.swift
+//  CollectionView
+//
+//  Created by Galina Iaroshenko on 08.12.2022.
+//
+
+import UIKit
+
+class NumberCell: UICollectionViewCell {
+    @IBOutlet var numberLabel: UILabel!
+    
+    
+    
+    
+    
+}
--- CollectionView/ViewController.swift
+++ CollectionView/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UIViewController, UICollectionViewDataSource {
+    
+    
+    let numbers: [Int] = (1...15).map{ $0 }
+    
+    
+    @IBOutlet var collectionView: UICollectionView!
+    /*
+    var myCollectionView:UICollectionView?
+       */
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+        }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return numbers.count
     }
-
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! NumberCell
+        let number = numbers[indexPath.item]
+        cell.numberLabel.text = "\(number)"
+        cell.contentView.layer.borderColor = UIColor.gray.cgColor
+        cell.contentView.layer.borderWidth = 1
+        return cell
+    }
+    
 }
+
 
